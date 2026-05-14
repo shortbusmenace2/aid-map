ace2a086532d6c65129900b1a803df033624d4d3
shortbusmenace2 <ecastella6@gmail.com>
2026-05-14 05:40:27 -0700

    Conflict zones updated 5/14

--- data.js
+++ data.js
@@ -9,7 +9,7 @@ const NGO_DATA = {
 
   "kherson": {
     name: "Kherson Oblast",
-    conflict: true,
+    conflict: false,
     ngos: [
       {
         name: "Médecins Sans Frontières (MSF)",
@@ -254,7 +254,7 @@ const NGO_DATA = {
 
   "mykolaiv": {
     name: "Mykolaiv Oblast",
-    conflict: true,
+    conflict: false,
     ngos: [
       {
         name: "UNICEF Ukraine",
@@ -341,7 +341,7 @@ const NGO_DATA = {
 
   "odesa": {
     name: "Odesa Oblast",
-    conflict: true,
+    conflict: false,
     ngos: [
       {
         name: "Razom for Ukraine",
@@ -430,7 +430,7 @@ const NGO_DATA = {
 
   "kyiv-city": {
     name: "Kyiv (City)",
-    conflict: true,
+    conflict: false,
     ngos: [
       {
         name: "United Help Ukraine",
@@ -525,7 +525,7 @@ const NGO_DATA = {
 
   "kyiv-obl": {
     name: "Kyiv Oblast",
-    conflict: true,
+    conflict: false,
     ngos: [
       {
         name: "Serhiy Prytula Charity Foundation",
@@ -667,7 +667,7 @@ const NGO_DATA = {
 
   "lviv": {
     name: "Lviv Oblast",
-    conflict: true,
+    conflict: false,
     ngos: [
       {
         name: "Lviv Volunteer Center",
@@ -746,7 +746,7 @@ const NGO_DATA = {
 
   "dnipro": {
     name: "Dnipropetrovsk Oblast",
-    conflict: true,
+    conflict: false,
     ngos: [
       {
         name: "East SOS",
@@ -872,7 +872,7 @@ const NGO_DATA = {
 
   "poltava": {
     name: "Poltava Oblast",
-    conflict: true,
+    conflict: false,
     ngos: [
       {
         name: "Caritas Ukraine",
@@ -903,7 +903,7 @@ const NGO_DATA = {
 
   "cherkasy": {
     name: "Cherkasy Oblast",
-    conflict: true,
+    conflict: false,
     ngos: [
       {
         name: "UN Development Programme Ukraine",
@@ -926,7 +926,7 @@ const NGO_DATA = {
 
   "vinnytsia": {
     name: "Vinnytsia Oblast",
-    conflict: true,
+    conflict: false,
     ngos: [
       {
         name: "International Organization for Migration (IOM)",
@@ -957,7 +957,7 @@ const NGO_DATA = {
 
   "zhytomyr": {
     name: "Zhytomyr Oblast",
-    conflict: true,
+    conflict: false,
     ngos: [
       {
         name: "Danish Refugee Council",
@@ -988,7 +988,7 @@ const NGO_DATA = {
 
   "chernihiv": {
     name: "Chernihiv Oblast",
-    conflict: true,
+    conflict: false,
     ngos: [
       {
         name: "ACTED Ukraine",
@@ -1074,7 +1074,7 @@ const NGO_DATA = {
 
   "kirovograd": {
     name: "Kirovohrad Oblast",
-    conflict: true,
+    conflict: false,
     ngos: [
       {
         name: "UNHCR Ukraine",
@@ -1090,7 +1090,7 @@ const NGO_DATA = {
   "rivne": { name: "Rivne Oblast", conflict: true, ngos: [] },
   "ternopil": {
     name: "Ternopil Oblast",
-    conflict: true,
+    conflict: false,
     ngos: [
       {
         name: "MoveUkraine",

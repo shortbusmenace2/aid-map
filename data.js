80c4ecbdfb630482a1a56f4c872302543bc98017
shortbusmenace2 <ecastella6@gmail.com>
2026-06-04 07:51:45 -0700

    Updated zones 6/4

--- data.js
+++ data.js
@@ -341,7 +341,7 @@ const NGO_DATA = {
 
   "odesa": {
     name: "Odesa Oblast",
-    conflict: false,
+    conflict: true,
     ngos: [
       {
         name: "Razom for Ukraine",
@@ -746,7 +746,7 @@ const NGO_DATA = {
 
   "dnipro": {
     name: "Dnipropetrovsk Oblast",
-    conflict: true,
+    conflict: false,
     ngos: [
       {
         name: "East SOS",
@@ -1074,7 +1074,7 @@ const NGO_DATA = {
 
   "kirovograd": {
     name: "Kirovohrad Oblast",
-    conflict: true,
+    conflict: false,
     ngos: [
       {
         name: "UNHCR Ukraine",
